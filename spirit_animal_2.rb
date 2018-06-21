544b65dc3e5f14823a277ee4d0ea3b268336de86
millie6000 <alohaturtle2003@gmail.com>
2018-06-21 19:52:36 +0000

    automatically backed up by learn

--- spirit_animal_2.rb
+++ spirit_animal_2.rb
@@ -1,37 +1,37 @@
 reqiure'colorize' 
-if "birthmonth" == "january"
+if birthmonth == "january"
   then "u are a polar bear".colorize(:)
 end 
-if "birthmonth" == "Febuary"
+if birthmonth == "Febuary"
   then "u r a wooly mammoth"
 end 
-if "birthmonth" == "March"
+if birthmonth == "March"
   then "u r a poision dart frog"
 end 
-if "birthmonth" == "April"
+if birthmonth == "April"
   then "u r a kangaroo"
 end 
-if "birthmonth" == "May"
+if birthmonth == "May"
   then "u r a fox"
 end 
-if "birthmonth" == "June"
+if birthmonth == "June"
   then "u r a blob fish"
 end 
-if "birthmonth" == "July"
-  then "u r a lion"
+if birthmonth == "July"
+  puts "u r a lion"
 end 
-if "birthmonth" == "August"
+if birthmonth == "August"
   then "u r a if phoenix"
 end
-if == "September"
+if birthmonth == "September"
   then "u r a dragon"
   ""
 end 
-if "birthmonth" == "October"
+if birthmonth == "October"
 then "u r a lobster moth"
 end
-if "birthmonth" == "November"
+if birthmonth == "November"
 then "u r a whale shark"
 end
-if "birthmonth" == "December"
+if birthmonth == "December"
   then "u r a snowy owl"
